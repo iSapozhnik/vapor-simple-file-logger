d2847be86c09f9756c3cf401ac75ce6dcb96e888
Damon <dong765@qq.com>
2022-01-12 17:35:31 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .library(name: "SimpleFileLogger", targets: ["SimpleFileLogger"])
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: 4.0.0")
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0")
     ],
     targets: [
         .target(name: "SimpleFileLogger", dependencies: ["Vapor"])
